bcc2f557f541552e0f9c7e4d2a51acc6cafb53d5
diaogege <215808446@qq.com>
2020-08-01 09:17:59 +0800

    提交信息

--- LabDAC/LabDAC/LabDAC.ip_user_files/sim_scripts/clk_division_0/xcelium/run.f
+++
@@ -1,8 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../../LabDAC.srcs/Frequency_Divider_Demo.srcs/sources_1/new/clk_division.v" \
-  "../../../../LabDAC.srcs/sources_1/ip/clk_division_0/sim/clk_division_0.v" \
--endlib
--makelib xcelium_lib/xil_defaultlib \
-  glbl.v
--endlib
-
